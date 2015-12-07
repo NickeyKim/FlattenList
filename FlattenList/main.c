4db72238f8b15b34c9223488280648ebaa76c882
NickeyKim <nakhoekim@nakhoes-macbook-pro.local>
2015-12-07 21:37:47 +0900

    Initial Commit

---
+++ FlattenList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FlattenList
+//
+//  Created by NAKHOE KIM on 12/7/15.
+//  Copyright © 2015 NAKHOE KIM. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
